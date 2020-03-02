7c8f4594df4d81998d5a3141786b3b0ff2d3802d
unknown <adamdefoe@gmail.com>
2020-03-01 19:46:29 -0600

    gwapes: mocking out username validator

--- test/validators/user/user.test.js
+++ test/validators/user/user.test.js
@@ -1,14 +1,26 @@
 describe('user validator tests', () => {
     let validator
+    let uv
     
     beforeEach(() => {
+        jest.resetModules()
+        jest.resetAllMocks()
+
+        jest.mock('../../../src/validators/user/username', () => ({
+            validate: jest.fn().mockName('uv.validate')
+        }))
+
         validator = require('../../../src/validators/user/user')
+        uv = require('../../../src/validators/user/username')
     })
 
     it('should successfully validate the user', () => {
         const user = {
             username: 'gwapes'
         }
+        uv.validate.mockImplementation((un, res) => {
+            return
+        })
 
         const actual = validator.validate(user)
 
