49958c0a308e99427d3d0ffe490f9b3c55623310
unknown <adamdefoe@gmail.com>
2020-03-02 18:29:37 -0600

    gwapes: adding in password validation and tests

--- src/validators/user/password.js
+++ src/validators/user/password.js
@@ -1,5 +1,34 @@
 const validate = (password, response) => {
+    if(!password){
+        response.isValid = false
+        response.messages.push('Password is a required field for account creation.')
+        return //if there is no string, there is nothing to keep validating
+    }
 
+    if(password.length < 8){
+        response.isValid = false
+        response.messages.push('Password must be at least 8 characters in length.')
+    }
+
+    if(!password.match(/(?=.*\d)/)){
+        response.isValid = false
+        response.messages.push('Password must contain at least on numeric character.')
+    }
+
+    if(!password.match(/(?=.*[a-z])/)){
+        response.isValid = false
+        response.messages.push('Password must contain at least one lowercase alphabetic character.')
+    }
+
+    if(!password.match(/(?=.*[A-Z])/)){
+        response.isValid = false
+        response.messages.push('Password must contain at least one uppercase alphabetic character.')
+    }
+
+    if(!password.match(/(?=.*\W)/)){
+        response.isValid = false
+        response.messages.push('Password must contain at least one special character.')
+    }
 }
 
 module.exports = { validate }
--- test/validators/user/password.test.js
+++ test/validators/user/password.test.js
@@ -4,4 +4,37 @@ describe('Password validator tests', () => {
     beforeEach(() => {
         validator = require('./../../../src/validators/user/password')
     })
+
+    it('should respond that password is valid', () => {
+        let actual = {
+            isValid: true,
+            messages: []
+        }
+
+        validator.validate('Good$9%1', actual)
+
+        expect(actual.isValid).toEqual(true)
+        expect(actual.messages).toStrictEqual([])
+    })
+
+    it.each([
+        ['string length', 'Good$9', ['Password must be at least 8 characters in length.']],
+        ['decimal character', 'Bad!@#$%', ['Password must contain at least on numeric character.']],
+        ['lowercase alpha character', 'BAD123#$%', ['Password must contain at least one lowercase alphabetic character.']],
+        ['uppercase alpha character', 'bad12#$%', ['Password must contain at least one uppercase alphabetic character.']],
+        ['special character', 'Bad12345', ['Password must contain at least one special character.']],
+        ['falsey check', '', ['Password is a required field for account creation.']],
+        ['falsey check', null, ['Password is a required field for account creation.']],
+        ['falsey check', undefined, ['Password is a required field for account creation.']]
+    ])('should fail on %s validation', (n, password, expectedMessage) => {
+        let actual = {
+            isValid: true,
+            messages: []
+        }
+
+        validator.validate(password, actual)
+
+        expect(actual.isValid).toEqual(false)
+        expect(actual.messages).toStrictEqual(expectedMessage)
+    })
 })
