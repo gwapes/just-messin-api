5dfc2f45eb1dbd11536a3f9a26baea23fef0b1d9
unknown <adamdefoe@gmail.com>
2020-03-03 22:01:42 -0600

    gwapes: switching from mongo save to insertOne as save is deprecated

--- src/data-access/accountData.js
+++ src/data-access/accountData.js
@@ -4,7 +4,7 @@ const saveUsers = async (user) => {
     const client = new MongoClient(`${process.env.ACCOUNT_DB_URL}`, { useUnifiedTopology: true })
     try{
         await client.connect()
-        var result = await client.db('account').collection('users').save(user)
+        var result = await client.db('account').collection('users').insertOne(user)
     }
     catch(ex) {
         console.error(ex)
