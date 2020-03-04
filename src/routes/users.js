d3f035551b57a285dbb4a32abbde4506558a28ab
unknown <adamdefoe@gmail.com>
2020-03-03 22:00:16 -0600

    gwapes: adding in catch all 500 response

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,17 +2,24 @@ const requestValidator = require('../validators/user/user')
 const accountDB = require('../data-access/accountData')
 
 const postUsers = async (req, res) => {
-    const validation = requestValidator.validate(req.body)
-    if(!validation.isValid){
-        res.status(400)
-        res.json({
-            errors: validation.messages
-        })
-    } else {
-        let result = await accountDB.saveUsers(req.body)
-        console.log(result)
-        res.status(200)
-        res.json(result)
+    try {
+        const validation = requestValidator.validate(req.body)
+        if (!validation.isValid) {
+            res.status(400)
+            res.json({
+                errors: validation.messages
+            })
+        } else {
+            let result = await accountDB.saveUsers(req.body)
+            console.log(result)
+            res.status(200)
+            res.json(result)
+        }
+    }
+    catch(ex) {
+        console.error(ex)
+        res.status(500)
+        res.json({ errors: [ 'Some technical issue occurred during processing.' ]})
     }
 }
 
